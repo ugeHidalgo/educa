39e1b51ddc1bd57c9d1c0531d9dbb73196192f2a
ugeHidalgo <ugehidalgo@hotmail.com>
2013-12-31 00:16:56 +0100

    Calculator class created

---
+++ UnitTests/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    class Calculator
+    {
+        private int _p1;
+        private int _p2;
+
+        public Calculator(int p1, int p2)
+        {
+           
+            _p1 = p1;
+            _p2 = p2;
+        }
+        internal int Add(int p1, int p2)
+        {
+            return p1 + p2;
+        }
+
+        internal int Substract(int p1, int p2)
+        {
+            return p1 - p2;
+        }
+    }
+}
---
+++ UnitTests/Tests/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Calculator aCalculator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            aCalculator = new Calculator(100,-100);
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            int result = aCalculator.Add(2, 2);
+            Assert.AreEqual(4,result);
+        }
+
+        [TestMethod]
+        public void Add2()
+        {
+            int result = aCalculator.Add(2, 5);
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void Substract()
+        {
+            int result = aCalculator.Substract(5, 3);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void SubstractExcedingLowerLimit()
+        {
+            int result = aCalculator.Substract(5, 3);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void SubstractWithNegativeResult()
+        {
+            int result = aCalculator.Substract(3, 5);
+            Assert.AreEqual(-2, result);
+        }
+        
+
+    }
+}
